d560d51241d612074d6e890fa3211fd898cc7408
Udit Sachdeva <uditsachdeva5@gmail.com>
2024-07-07 14:26:49 -0400

    models created

--- public/js/login.js
+++ public/js/login.js
@@ -2,10 +2,10 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Collect values from the login form
-    const email = document.querySelector("#email-login").value.trim();
+    const userName = document.querySelector("#user-name").value.trim();
     const password = document.querySelector("#password-login").value.trim();
 
-    if (email && password) {
+    if (userName && password) {
         // Send a POST request to the API endpoint
         const response = await fetch("/api/users/login", {
             method: "POST",
